e0e049acfc1da0e18623046b7600a5e8cf4328a4
Maksim Litvinov <metalim@gmail.com>
2024-07-20 18:31:21 +0300

    handle GET and SET

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -28,6 +28,7 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
+	mem := map[string]string{}
 	for {
 		_, err := conn.Read(buf)
 		if err != nil {
@@ -46,14 +47,36 @@ func handleConnection(conn net.Conn) {
 		}
 		command := strings.ToUpper(lines[2])
 		switch command {
+
 		case "PING":
 			_, err = conn.Write([]byte("+PONG\r\n"))
+
 		case "ECHO":
 			if len(lines) < 4 {
 				fmt.Println("Invalid command")
 				return
 			}
 			_, err = conn.Write([]byte("+" + lines[4] + "\r\n"))
+
+		case "SET":
+			if len(lines) < 6 {
+				fmt.Println("Invalid command")
+				return
+			}
+			mem[lines[4]] = lines[6]
+			_, err = conn.Write([]byte("+OK\r\n"))
+
+		case "GET":
+			if len(lines) < 4 {
+				fmt.Println("Invalid command")
+				return
+			}
+			val, ok := mem[lines[4]]
+			if !ok {
+				_, err = conn.Write([]byte("$-1\r\n"))
+				break
+			}
+			_, err = conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
 		}
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
