19e9dae7fe146dd69023d587da1512106c4f4994
Maksim Litvinov <metalim@gmail.com>
2024-07-20 20:54:50 +0300

    change port

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,9 +12,13 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := "6379"
+	if len(os.Args) > 2 && os.Args[1] == "--port" {
+		port = os.Args[2]
+	}
+	l, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port " + port)
 		os.Exit(1)
 	}
 	for {
